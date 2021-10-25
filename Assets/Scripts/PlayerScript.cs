d6c473c61e4705b42dc1345b85574705a93df2b7
mlouis01 <mudith.louis@knights.ucf.edu>
2021-10-24 23:15:27 -0400

    changes

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,14 +11,36 @@ public class PlayerScript : MonoBehaviour
 
     public Text score;
 
+    public Text countText;
+
+    public Text winText;
+
+    public Text lives;
+
+    private int livesValue = 3;
+
     private int scoreValue = 0;
+
+    private int count;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        lives.text = "Lives: " + livesValue.ToString();
+        count = 0;
+        SetCountText ();
     }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
 
     // Update is called once per frame
     void FixedUpdate()
@@ -35,8 +57,18 @@ public class PlayerScript : MonoBehaviour
         {
             scoreValue += 1;
             score.text = scoreValue.ToString();
+            count = count + 1;
+            SetCountText ();
             Destroy(collision.collider.gameObject);
         }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = "Lives: " + livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -48,12 +80,14 @@ public class PlayerScript : MonoBehaviour
             rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
         }
         }
+    }
+
+    void SetCountText ()
+    {
+        if (count >= 4)
+        {
+            winText.text = "You Win! Game created by Mudith Louis";
+        }
     
-    {
-        if (Input.GetKey("escape"))
-    {
-    Application.Quit();
-    }
-    }
     }
 }
